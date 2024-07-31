453e8afe55fbc7fa8143d052c365dc6ed7a6c004
cleverpuppy <919479850@qq.com>
2024-07-31 23:13:22 +0800

    feature: multi threads

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use clap::Parser;
 use std::{
     io::Write,
     process::{Command, Stdio},
+    sync::{
+        atomic::{AtomicU32, Ordering},
+        Arc, Mutex,
+    },
+    thread,
 };
 
 /// tester: A simple cli tool to help you run a test multi times
@@ -16,54 +21,111 @@ struct Cli {
     score: bool,
     /// Number of times to run the commands
     #[arg(short = 'n')]
-    times: u8,
+    times: u32,
+    /// Number of threads
+    #[arg(short = 'p')]
+    threads: u8,
     exec: String,
     exec_args: Vec<String>,
 }
 
+struct TesterInfo {
+    fail_times: AtomicU32,
+    run_times: AtomicU32,
+    total_scores: Mutex<f64>,
+    cli_args: Cli,
+}
+
+impl TesterInfo {
+    fn print_summary(&self) {
+        let fail_times = self.fail_times.load(Ordering::SeqCst);
+        let run_times = self.run_times.load(Ordering::SeqCst);
+        let total_score = *self.total_scores.lock().unwrap();
+        if fail_times > 0 {
+            println!("#tester finished. Failed {} / {}", fail_times, run_times);
+        } else {
+            println!("#tester finished. No failure in {} runs.", run_times);
+            if self.cli_args.score {
+                let avg_score = total_score / run_times as f64;
+                println!("#tester average score: {}.", avg_score);
+            }
+        }
+    }
+
+    fn do_test(&self, times: u32) {
+        let mut run_times = 0;
+        let mut fail_times = 0;
+        let mut total_scores = 0.0;
+        let mut program = Command::new(&self.cli_args.exec);
+        program
+            .args(&self.cli_args.exec_args)
+            .stderr(Stdio::piped())
+            .stdout(Stdio::piped());
+        for _ in 0..times {
+            let p_instance = program.spawn().expect("cmd failed to start");
+            let p_ret = p_instance.wait_with_output().expect("Unable to run cmd");
+            if !p_ret.status.success() {
+                fail_times += 1;
+            }
+            if !self.cli_args.silent {
+                let mut stdout = std::io::stdout();
+                let mut stderr = std::io::stderr();
+                stdout.write_all(&p_ret.stdout).unwrap();
+                stderr.write_all(&p_ret.stderr).unwrap();
+            }
+
+            if self.cli_args.score {
+                let std_out = String::from_utf8(p_ret.stdout).unwrap();
+                let score: f64 = std_out.trim().parse().unwrap();
+                total_scores += score;
+            }
+            run_times += 1;
+        }
+        self.append_result(run_times, fail_times, total_scores);
+    }
+
+    fn append_result(&self, run_times: u32, fail_times: u32, total_scores: f64) {
+        self.run_times.fetch_add(run_times, Ordering::SeqCst);
+        self.fail_times.fetch_add(fail_times, Ordering::SeqCst);
+        if self.cli_args.score {
+            *self.total_scores.lock().unwrap() += total_scores;
+        }
+    }
+}
+
 fn main() {
-    let cli_args = Cli::parse();
+    let test_info = Arc::new(TesterInfo {
+        fail_times: AtomicU32::new(0),
+        run_times: AtomicU32::new(0),
+        total_scores: Mutex::new(0.0),
+        cli_args: Cli::parse(),
+    });
 
-    let mut program = Command::new(&cli_args.exec);
-    program
-        .args(&cli_args.exec_args)
-        .stderr(Stdio::piped())
-        .stdout(Stdio::piped());
-
-    // println!("#tester: Begin to run {:#?}", program);
-    // println!("#Cliargs : {:#?}", cli_args);
-
-    let mut fail_times = 0;
-    let mut run_times = 0;
-    let mut total_scores = 0.0;
-    for _ in 0..cli_args.times {
-        let p_instance = program.spawn().expect("cmd failed to start");
-        let p_ret = p_instance.wait_with_output().expect("Unable to run cmd");
-        if !p_ret.status.success() {
-            fail_times += 1;
-        }
-        if !cli_args.silent {
-            let mut stdout = std::io::stdout();
-            let mut stderr = std::io::stderr();
-            stdout.write_all(&p_ret.stdout).unwrap();
-            stderr.write_all(&p_ret.stderr).unwrap();
+    let mut handles = vec![];
+    let threads = std::cmp::max(test_info.cli_args.threads, 1);
+    let times_per_thread = test_info.cli_args.times / threads as u32;
+    let mut times_extra = test_info.cli_args.times % threads as u32;
+    for _ in 0..threads {
+        let mut times_this_thread = times_per_thread;
+        if times_extra > 0 {
+            times_this_thread += 1;
+            times_extra -= 1;
         }
 
-        if cli_args.score {
-            let std_out = String::from_utf8(p_ret.stdout).unwrap();
-            let score: f64 = std_out.trim().parse().unwrap();
-            total_scores += score;
+        if times_this_thread <= 0 {
+            break;
         }
-        run_times += 1;
+
+        let test_info_share = test_info.clone();
+        let handle = thread::spawn(move || {
+            test_info_share.do_test(times_per_thread);
+        });
+        handles.push(handle);
     }
 
-    if fail_times > 0 {
-        println!("#tester finished. Failed {} / {}", fail_times, run_times);
-    } else {
-        println!("#tester finished. No failure in {} runs.", run_times);
-        if cli_args.score {
-            let avg_score = total_scores / f64::from(run_times);
-            println!("#tester average score: {}.", avg_score);
-        }
+    for handle in handles {
+        handle.join().unwrap();
     }
+
+    test_info.print_summary();
 }
